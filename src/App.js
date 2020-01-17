df8bd0d8417ff0707eabb5b2bb73f0c6b3f8b33d
zyberninja <zyberdev@gmail.com>
2020-01-16 20:25:46 -0500

    Adding all files of first homework

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() 
+{
+  return (
+    <div className="App">
+      {/*
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      https://i.vimeocdn.com/video/646683867.webp?mw=1000&mh=563&q=70
+
+
+      */}
+          <div class = "Background-image">
+            <div class ="textinfo">
+                <h1 class2>Title</h1>
+                <h2 class2>Sub title</h2>
+                <p>
+                  xyz stuff
+                  </p>
+                  <ul>
+                    <li>test1</li>
+                    <li>test2</li>
+                  </ul>
+                  <p>footer</p>
+              </div>
+          </div>
+
+
+
+    </div>
+  
+  );
+}
+
+export default App;
