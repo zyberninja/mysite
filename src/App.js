d3bbaaa5092f024c73ed3b182d6ace45ef619290
zyberninja <zyberdev@gmail.com>
2020-01-18 15:05:56 -0500

    Updated information

--- src/App.js
+++ src/App.js
@@ -7,23 +7,10 @@ function App()
   return (
     <div className="App">
       {/*
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      https://i.vimeocdn.com/video/646683867.webp?mw=1000&mh=563&q=70
-
+      Name:Jorge Perez 
+      Panther Id: 1288448
+      Project IP Address: 18.207.211.37:3000 
+      jpere136@fiu.edu
 
       */}
           <div class = "Background-image">
